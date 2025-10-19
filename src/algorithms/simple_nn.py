e23e0b03367bcd7b83aa2a24a02443a06eca95fd
Simonexc <szymekdt@gmail.com>
2025-10-19 23:48:39 +0200

    Fix simple nn

--- src/algorithms/simple_nn.py
+++ src/algorithms/simple_nn.py
@@ -305,9 +305,8 @@ class ActorHead(nn.Module):
                 nn.ReLU(),
                 nn.Linear(hidden_size, action_spec.high[0] + 1),
             )
-            self.actor_heads = nn.ModuleList([self.actor_head])
         else:
-            self.actor_heads = nn.ModuleList([
+            self.actor_head = nn.ModuleList([
                 nn.Sequential(
                     nn.Linear(embedding_size, hidden_size),
                     nn.ReLU(),
@@ -337,7 +336,7 @@ class ActorHead(nn.Module):
         is_grad = x.requires_grad
 
         outputs = []
-        for i, head in enumerate(self.actor_heads):
+        for i, head in enumerate(self.actor_head if isinstance(self.actor_head, nn.ModuleList) else [self.actor_head]):
             head_output = head(x.to(self.device))
             head_output[~actions_mask[..., self.player_start_id + i, :]] = -1e8  # Mask invalid actions
             if not is_grad:
